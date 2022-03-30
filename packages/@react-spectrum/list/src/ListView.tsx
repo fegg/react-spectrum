e037e418d5a2174fee19db9103a9175aed61ade0
Daniel Lu <dl1644@gmail.com>
2022-03-29 17:01:23 -0700

    cleanup

--- packages/@react-spectrum/list/src/ListView.tsx
+++ packages/@react-spectrum/list/src/ListView.tsx
@@ -150,7 +150,8 @@ function ListView<T extends object>(props: ListViewProps<T>, ref: DOMRef<HTMLDiv
     ref: domRef,
     direction,
     collator,
-    focusMode: 'row'
+    focusMode: 'row',
+    skipCell: true
   }), [state, domRef, direction, collator]);
 
   let provider = useProvider();
